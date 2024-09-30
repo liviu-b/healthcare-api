08418cf337a07824ecdafef74642811ad28db865
Liviu <liviu.bancila95@gmail.com>
2024-09-30 10:01:22 +0300

    add db and update main file

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const patientRoutes = require('./routes/patientRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const requestLogger = require('./middleware/requestLogger');
+const connectDB = require('./config/db');
 
 
 const app = express();
@@ -15,9 +16,12 @@ app.use('/api/patients', patientRoutes);
 app.use(requestLogger);  // Apply request logger to all incoming requests
 app.use(errorHandler);  // Use error handler after all routes
 
+
+
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
+connectDB();
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
